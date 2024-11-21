e6beb0b92bbe840212d7ddc9bb56f6ff54ae41ee
Aïmen <aimen2nice@gmail.com>
2024-11-21 17:37:01 +0100

    heo banniere ok

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,7 @@
 import hero from "../assets/images/hero.png"
+import plus from "../assets/images/plus.svg"
+import play from "../assets/images/play.svg"
+import narcos from "../assets/images/narcos.svg"
 import "../assets/HeroSection.css"
 function HeroSection() {
     return(
@@ -9,14 +12,15 @@ function HeroSection() {
             <div className="text">
                 <span className="netflix">NETFLIX</span>
                 <span className="original">Original</span>
-                <h2>NARCOS</h2>
+                <div className="logo-narcos"><img src={narcos} alt="narcos-logo"/></div>
                 <h3>Regardez la saison 3 maintenant</h3>
                 <p>Le cartel de Cali reprend le pouvoir en Colombie.
                     Les successeurs d’Escobar passent à l’action et
                     déclarent la guerre au gouvernement.
                 </p>
                 <div className="buttons">
-                    <div className="butt"></div>
+                    <div className="btn"><img src={play} alt="play icone"/>Lecture</div>
+                    <div className="btn"><img src={plus} alt="plus icone"/>My List</div>
                 </div>
 
             </div>
