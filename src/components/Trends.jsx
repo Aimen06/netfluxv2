5f40788e723534c81902986ff9120f0d36b1ca30
Aïmen <aimen2nice@gmail.com>
2024-12-03 14:21:34 +0100

    movidb trend ok

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,13 +1,30 @@
 import "../assets/Gallery.css"
-function Trends(props)
+import {useState, useEffect} from 'react';
+function Trends()
 {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OWQ4OGM1Njg1NWUyOTg2ZDdkNjJiZTJhMDQ2MWNkZiIsIm5iZiI6MTczMjg3Nzk0NS45NTUwMDAyLCJzdWIiOiI2NzQ5OWU3OTE2NTBhNWUzNTMzOGZhZjUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mtYSaLxndlH5wvQB9Pb1_IEn37u8qa5OIa8AAxhmDCc'
+        }
+    };
+        const urlImage = 'https://image.tmdb.org/t/p/w500/'
+        const [movies, setMovies] = useState([]);
+        useEffect( () => {
+            fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', options)
+                .then((response) => response.json())
+                .then((data)=> setMovies(data.results))
+                .catch(err => console.error(err));
+            },[]);
+
     return (
         <>
         <div className="tendance">Tendances actuelles</div>
          <div className="row">
-             {props.images.map(function (element) {
+             {movies.map(function (movie) {
                 return (<div className="images">
-                    <img src={element} alt={element}/>
+                    <img src={urlImage+movie.poster_path} alt={urlImage+movie.poster_path}/>
                 </div>)
             })}
         </div>
